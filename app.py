fe822de482a5fc39254b460c94a24dcc57a98cb3
Amir Sasson <Amirsas@gmail.com>
2017-02-06 18:32:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -112,6 +112,7 @@ def get_ammotion_by_image():
 def get_stats():  
     return "OK!!"
 
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
